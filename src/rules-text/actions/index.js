0141b7a65c8c591fd207c16a78f671b0987845cc
Lipokurto <lighting13@yandex.ru>
2023-02-10 18:08:00 +0300

    add action cards

---
+++ src/rules-text/actions/index.js
@@ -0,0 +1,10 @@
+export { tithe } from './tithe';
+export { assault } from './assault';
+export { search } from './search';
+export { prison } from './prison';
+export { ransom } from './ransom';
+export { globalPray } from './global-pray';
+export { pray } from './pray';
+export { fire } from './fire';
+export { eat } from './eat';
+export { ham } from './ham';
