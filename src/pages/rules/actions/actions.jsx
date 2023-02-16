d2260b28a0468126ed9c6e1c1a86e444afb46005
Lipokurto <lighting13@yandex.ru>
2023-02-16 13:14:57 +0300

    add accordion on actions

--- src/pages/rules/actions/actions.jsx
+++ src/pages/rules/actions/actions.jsx
@@ -1,70 +1,43 @@
 import React from "react";
+import { Accordion, AccordionItem } from 'react-sanfona';
 
-import { ItemModal, LinkButton } from "../../../components";
 import { tithe, assault, search, prison, ransom, pray, fire, globalPray, eat, ham } from "../../../rules-text/actions";
 
 import s from './actions.module.css';
 
+function AItem(item) {
+  return (
+    <AccordionItem 
+      title={item.label}
+      titleClassName={s.title}
+    >
+      <div className={s.details}>
+        {item.text}
+      </div>
+    </AccordionItem>
+  )
+}
+
 export function Actions() {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const [item, setItem] = React.useState(null);
-
-  const handleClick = React.useCallback((item) => {
-    setItem(item);
-    setIsOpen(true);
-  }, []);
-
   return (
     <>
       <div className={s.container}>
         <h1>Игровые действия</h1>
-        <div className={s.block}>
-          <h2><LinkButton text='Церковная десятина' hint={tithe.hint} onclick={() => handleClick(tithe)}/></h2>
-        </div>
 
-        <div className={s.block}>
-          <h2><LinkButton text='Штурм лагеря' hint={assault.hint} onclick={() => handleClick(assault)}/></h2>
-        </div>
+        <Accordion>
+          {AItem(tithe)}
+          {AItem(assault)}
+          {AItem(search)}
+          {AItem(prison)}
+          {AItem(ransom)}
+          {AItem(pray)}
+          {AItem(globalPray)}
+          {AItem(fire)}
+          {AItem(eat)}
+          {AItem(ham)}
 
-        <div className={s.block}>
-          <h2><LinkButton text='Обыск лагеря' hint={search.hint} onclick={() => handleClick(search)}/></h2>
-        </div>
-
-        <div className={s.block}>
-          <h2><LinkButton text='Пленение' hint={prison.hint} onclick={() => handleClick(prison)}/></h2>
-        </div>
-
-        <div className={s.block}>
-          <h2><LinkButton text='Выкуп пленника' hint={ransom.hint} onclick={() => handleClick(ransom)}/></h2>
-        </div>
-
-        <div className={s.block}>
-          <h2><LinkButton text='Молитва' hint={pray.hint} onclick={() => handleClick(pray)}/></h2>
-        </div>
-
-        <div className={s.block}>
-          <h2><LinkButton text='Вселенская молитва' hint={globalPray.hint} onclick={() => handleClick(globalPray)}/></h2>
-        </div>
-
-        <div className={s.block}>
-          <h2><LinkButton text='Отчищение огнем' hint={fire.hint} onclick={() => handleClick(fire)}/></h2>
-        </div>
-
-        <div className={s.block}>
-          <h2><LinkButton text='Пожирание' hint={eat.hint} onclick={() => handleClick(eat)}/></h2>
-        </div>
-
-        <div className={s.block}>
-          <h2><LinkButton text='Заключение в темницу' hint={ham.hint} onclick={() => handleClick(ham)}/></h2>
-        </div>
+        </Accordion>
       </div>
-
-      {isOpen && (
-        <ItemModal
-          setIsOpen={setIsOpen}
-          text={item}
-        />
-      )}
     </>
   )
 }
