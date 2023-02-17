5b9876f935feefe723fb7a91e77414ed17aad4a5
Lipokurto <lighting13@yandex.ru>
2023-02-17 22:06:15 +0300

    finished: actions accordion, redo: actions items structure

--- src/pages/rules/actions/actions.jsx
+++ src/pages/rules/actions/actions.jsx
@@ -1,17 +1,61 @@
 import React from "react";
 import { Accordion, AccordionItem } from 'react-sanfona';
 
+import arrowDown from '../../../assets/icons/icon-arrow-down.svg';
+import arrowUp from '../../../assets/icons/icon-arrow-up.svg';
+import { Chapter } from "../../../components/chapter/chapter";
 import { tithe, assault, search, prison, ransom, pray, fire, globalPray, eat, ham } from "../../../rules-text/actions";
 
 import s from './actions.module.css';
 
-function AItem(item) {
+function makeArrow(isLeft, isOpen) {
+  if (isOpen) {
+    return (
+      <div className={isLeft ? s.arrow_left_up : s.arrow_right_up}>
+        <img src={arrowUp} alt='Icons' />
+      </div>
+    )
+  }
+
   return (
-    <AccordionItem 
-      title={item.label}
-      titleClassName={s.title}
+    <div className={isLeft ? s.arrow_left_down : s.arrow_right_down}>
+      <img src={arrowDown} alt='Icons' />
+    </div>
+  )
+}
+
+function ActionItem(item) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleExpand = React.useCallback(() => {
+    return setIsOpen(true);
+  }, []);
+  
+  const handleClose = React.useCallback(() => {
+    if (isOpen) return setIsOpen(false);
+  }, [isOpen]);
+
+  const formedLabel = React.useMemo(() => {
+    return (
+      <div className={isOpen ? s.label_open : s.label}>
+        {makeArrow(true, isOpen)}
+
+        <h3>
+          {item.label}
+        </h3>
+
+        {makeArrow(false, isOpen)}
+      </div>
+    )
+  }, [isOpen, item.label]);
+
+  return (
+    <AccordionItem
+      title={formedLabel}
+      onExpand={() => handleExpand()}
+      onClose={() => handleClose()}
     >
-      <div className={s.details}>
+      <div className={isOpen ? s.item : undefined}>
         {item.text}
       </div>
     </AccordionItem>
@@ -22,21 +66,35 @@ export function Actions() {
   return (
     <>
       <div className={s.container}>
-        <h1>Игровые действия</h1>
+        <Chapter chapter='ИГРОВЫЕ ДЕЙСТВИЯ' />
 
-        <Accordion>
-          {AItem(tithe)}
-          {AItem(assault)}
-          {AItem(search)}
-          {AItem(prison)}
-          {AItem(ransom)}
-          {AItem(pray)}
-          {AItem(globalPray)}
-          {AItem(fire)}
-          {AItem(eat)}
-          {AItem(ham)}
+        <div className={s.block}>
+          <h2>Боевые действия</h2>
+          <Accordion>
+            {ActionItem(prison)}
+            {ActionItem(ransom)}
+            {ActionItem(assault)}
+            {ActionItem(search)}
+          </Accordion>
+        </div>
 
-        </Accordion>
+        <div className={s.block}>
+          <h2>Религиозные действия</h2>
+            <Accordion>
+              {ActionItem(pray)}
+              {ActionItem(fire)}
+              {ActionItem(tithe)}
+              {ActionItem(globalPray)}
+            </Accordion>
+        </div>
+
+        <div className={s.block}>
+          <h2>Чудовищные действия</h2>
+            <Accordion>
+              {ActionItem(eat)}
+              {ActionItem(ham)}
+            </Accordion>
+        </div>
       </div>
     </>
   )
