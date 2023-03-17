42f4c2a440b93aecc3c115dc4e9935eb164ee8fc
Lipokurto <lighting13@yandex.ru>
2023-03-17 20:51:24 +0300

    fix hits cal & batle page

--- src/pages/help/char-calc/char-calc.tsx
+++ src/pages/help/char-calc/char-calc.tsx
@@ -1,7 +1,11 @@
 import { HitsCalc } from "../../../components";
 
+import s from './char-calc.module.css';
+
 export function CharCalc(): JSX.Element {
   return (
+    <div className={s.container}>
       <HitsCalc />
+    </div>
   );
 }
