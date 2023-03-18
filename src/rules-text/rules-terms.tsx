86a7c957ece634d2ef645590d33a5adaa2491d0d
Lipokurto <lighting13@yandex.ru>
2023-03-18 14:24:07 +0300

    add registry

---
+++ src/rules-text/rules-terms.tsx
@@ -0,0 +1,12 @@
+import { Item } from './type';
+
+export const rulesTerms: Item = {
+  label: 'Уловия проведения мероприятия',
+  element:
+    <>
+      <div>Какать в туалет</div>
+      <div>Не нажираться как скот</div>
+      <div>Вести себя хорошо</div>
+      <div>Получать удовольствия от мероприятия</div>
+    </>
+}
