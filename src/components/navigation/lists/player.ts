e53398d314ba8150e69e85aa0a9883235e8e19f5
Lipokurto <lighting13@yandex.ru>
2023-03-25 10:04:15 +0300

    start players map

--- src/components/navigation/lists/player.ts
+++ src/components/navigation/lists/player.ts
@@ -5,4 +5,8 @@ export const player: ListItem[] = [
     label: 'Регистрация на игру',
     link: '/registration',
   },
+  {
+    label: 'Карта подготовки игрока',
+    link: '/map-support',
+  },
 ]
