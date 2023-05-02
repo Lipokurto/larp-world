3ecb939c19e54d05229ff1c7f5f6da51be14768e
Lipokurto <lighting13@yandex.ru>
2023-05-02 16:19:21 +0300

    finshed history

---
+++ src/history/holy-city.tsx
@@ -0,0 +1,32 @@
+import { Item } from "../rules-text/type";
+
+import holyCoat from '../assets/coats/holy.png'; 
+
+import s from './history.module.css';
+
+export const holyHistory: Item = {
+  label: 'Герцогство Гранта',
+  element:
+    <>
+      <div className={s.info}>
+        <div className={s.coat}>
+          <img src={holyCoat} alt='' width='200'/>
+        </div>
+
+        <div className={s.text}>
+          <div><i>Правитель: </i>Пантифик</div>
+          <div><i>Столица: </i>город Святой город</div>
+          <div><i>Герб: </i>серебряный и золотой ключ на алом фоне</div>
+          <div><i>Цвета: </i>белый и красный для верховных служителей, черный и серый для низших чинов</div>
+        </div>
+      </div>
+
+    <div className={s.block2}>
+      <h3>Краткая история</h3>
+      <div>Теократический город-государство, который ведет борьбу за распространение престольной веры</div>
+      <div>Активно противостоит политеизму Кушан</div>
+      <div>Обладает минимальным количеством войск, живет за счет церковной десятины, которую собирает почти со всей территории континента</div>
+      <div>Политические придерживается нейтралитета</div>
+    </div>
+  </>
+}
