eab5707cb8d12e3493f2c99151541fe207a24aef
Lipokurto <lighting13@yandex.ru>
2023-09-12 16:15:36 +0300

    add back toolTip

--- src/components/card-tooltip/card-tooltip.tsx
+++ src/components/card-tooltip/card-tooltip.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Tooltip from "react-tooltip-lite";
 
 type Props = {
@@ -7,9 +8,17 @@ type Props = {
 }
 
 export function CardTooltip(props: Props): JSX.Element {
+  const renderContent = React.useMemo(() => {
+    if (props.cardName === 'Спина') {
+      return <div>Защищена ли ваша спина броней?</div>
+    }
+
+    return <img src={props.src} alt='' width={props.width || 200} />
+  }, [props.cardName, props.src, props.width]);
+
   return (
     <Tooltip
-      content={<img src={props.src} alt='' width={props.width || 200} />}
+      content={renderContent}
       background='wheat'
     >
       <div style={{ textDecoration: 'underline' }}>{props.cardName}</div>
