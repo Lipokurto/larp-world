275df0921a1d945176df0be0c669fc1dfb148578
Lipokurto <lighting13@yandex.ru>
2023-11-09 11:33:24 +0300

    remove visual battles

--- src/pages/help/index.ts
+++ src/pages/help/index.ts
@@ -2,6 +2,4 @@ export { CharCalc } from './char-calc/char-calc';
 export { BuildCalc } from './build-calc/build-calc';
 export { HealCalc } from './heal-calc/heal-calc';
 export { MonsterCalc } from './monster-calc/monster-calc';
-export { FinalCalc } from './final-calc/final-calc';
-export { AssaultCalc } from './assault-calc/assault-calc';
 export { HouseCalc } from './house-calc/house-calc';
