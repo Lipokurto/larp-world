de566b7d89deb012c5440abbfa11e61b4018fec6
Lipokurto <lighting13@yandex.ru>
2023-11-09 10:17:22 +0300

    fix wording

--- src/rules-text/psy/level-01/critic.tsx
+++ src/rules-text/psy/level-01/critic.tsx
@@ -3,5 +3,5 @@ import { Item } from '../../type';
 export const critic: Item = {
   label: 'Критик',
   psyLevel: 1,
-  element: <div>Не может молчать о том что все вокруг деликатны.</div>
+  element: <div>Не может молчать о том что все вокруг дилетанты.</div>
 }
--- src/rules-text/psy/level-02/disgust.tsx
+++ src/rules-text/psy/level-02/disgust.tsx
@@ -1,7 +1,7 @@
 import { Item } from '../../type';
 
 export const disgust: Item = {
-  label: 'Брезгливость>',
+  label: 'Брезгливость',
   psyLevel: 2,
   element: <div>Вы не можете посещать увеселительные заведения ниже 3 уровня качества.</div>
 }
