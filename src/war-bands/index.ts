880e63cbc0df7fd239275580d1d91dddabae02c8
Lipokurto <lighting13@yandex.ru>
2024-12-17 13:35:13 +0300

    remove some pics

--- src/war-bands/index.ts
+++ src/war-bands/index.ts
@@ -1,8 +1,4 @@
 export { chainsOrder } from './neutral/chains';
-export { dogs } from './tudor/dogs';
-export { blades } from './midlend/blades';
-export { axes } from './midlend/axes';
 export { holyPoor } from './midlend/holy-poor';
 export { bear } from './tudor/bear';
 export { outlanders } from './neutral/outlanders';
-export { knights } from './tudor/knights';
