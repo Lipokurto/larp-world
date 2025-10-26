23c638d9e23f9512d04eac8c8173b7835d56a238
Lipokurto <lighting13@yandex.ru>
2025-10-26 16:23:10 +0300

    redesign main page

--- src/components/logo/eclipse/eclipse.tsx
+++ src/components/logo/eclipse/eclipse.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import tri from '../../../assets/triangle.svg';
-import knight from '../../../assets/knight.svg';
+import knight from '../../../assets/knight_02.png';
 
 import s from './eclipse.module.css';
 
@@ -10,7 +10,7 @@ export function Eclipse(): JSX.Element {
     <div className={s.mark__container}>
       <div className={s.circle}>
         <div className={s.knight}>
-          <img src={knight} alt='Knight' />
+          <img src={knight} alt='Knight' height={500}/>
         </div>
         <img src={tri} alt='Triangle'/>
       </div>
