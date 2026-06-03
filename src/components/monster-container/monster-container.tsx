0970f4cbfc9ea9ff3343b52fa5defa4530e25075
Lipokurto <lighting13@yandex.ru>
2026-06-03 19:36:28 +0300

    fix monsters adaptive

--- src/components/monster-container/monster-container.tsx
+++ src/components/monster-container/monster-container.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { useResize } from '../utils/use-resize';
+
 import s from './monster-container.module.css';
 
 type Props = {
@@ -11,14 +13,18 @@ type Props = {
 }
 
 export function MonsterContainer({ icon, text, damage, hits, special }: Props): JSX.Element {
+  const { width } = useResize();
+
   return (
     <div className={s.container}>
-      <img src={icon} alt='' width={400}/>
+      <img src={icon} alt='' width={width < 1024 ? 350 : 500}/>
       <div><i>Тип атаки:&ensp;</i><span style={{ color: damage === 'Особое оружие' ? 'red' : 'white' }}>{damage}</span></div>
       <div><i>Живучесть:&ensp;</i><span style={{ color: hits === 'Высокая' ? 'red' : 'white' }}>{hits}</span></div>
+
       {special && (
         <div><i>Особенность:&ensp;</i><span style={{ color: 'orange' }}>{special}</span></div>
       )}
+
       <br />
       <div>{text}</div>
     </div>
